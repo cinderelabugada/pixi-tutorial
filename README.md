a84531bb1f94cdd24b081cb71624bfcaea354c23
Cinderela Bugada <bugadacinderela@gmail.com>
2021-02-17 15:23:33 -0300

    Correção do problema com o parcel

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ Projeto senguindo o tutorial do Pixi. O [PixiJS](https://www.pixijs.com/) é uma
 ## Referências:
 
 Página do PixiJS: [https://www.pixijs.com/](https://www.pixijs.com/)
+
 Galeria de aplicações: [https://www.pixijs.com/gallery](https://www.pixijs.com/gallery)
+
 Exemplo de aplicação: [Recollection](http://because-recollection.com/)
 
+Documentação: [PixiJS](https://pixijs.download/dev/docs/index.html)
 
 ### Seção Getting Started
 Primeiro tutorial: [01 - Getting started with PixiJS](https://github.com/kittykatattack/learningPixi)
--- src/index.js
+++ src/index.js
@@ -1,58 +1,125 @@
-///import * as PIXI from 'pixi.js'
-import * as PIXI from './pixi/pixi.min.js'
+import * as PIXI from 'pixi.js'
 import scaleToWindow from './scaleToWindow'
 
-import characterImage from './images/arara.png'
+import araraImg from './images/arara.png'
+import megamanImg from './images/megaman.png'
+import luigiImg from './images/luigi.png'
 
-// Cria um novo objeto do tipo Application que cria um canvas para HTML
+/**
+ * Cria um novo objeto do tipo Application que cria um canvas para HTML
+ */
 const app = new PIXI.Application({ 
   width: window.innerWidth,         // default: 800
   height: window.innerHeight,        // default: 600
   antialias: true,    // default: false
-  transparent: true,  // default: false
+  transparent: false,  // default: false
   resolution: 1       // default: 1
 });
 
-// Modifica cor de fundo
+/**
+ * Modifica cor de fundo
+ */
 //app.renderer.backgroundColor = 0x061639;
 
-// Modifica o tamanho e disposição do canvas
+/**
+ * Modifica o tamanho e disposição do canvas
+ */
 app.renderer.view.style.position = 'absolute';
 app.renderer.view.style.display = 'block';
 app.renderer.autoResize = true;
 
-// Atualiza tamanho do canvas responsivamente
+/**
+ * Atualiza tamanho do canvas responsivamente
+ */
 window.addEventListener('resize', function(event){ 
   scaleToWindow(app.renderer.view);
 })
 
-// Adiciona o canvas na página HTML
+/**
+ * Adiciona o canvas na página HTML
+ */
 document.body.appendChild(app.view);
 
 /**
  * Carrega os sprites e converte para o formato cache textura utilizado pelo WebGL
  */
-
-console.log(app)
-
-const loader = PIXI.Loader.shared
-//const loader = new PIXI.Loader()
-
-app.loader.add('images/megaman.png')
+PIXI.Loader.shared
+  .add([
+    araraImg,
+    megamanImg,
+    luigiImg
+  ])
   .load(setup)
 
-  function setup (loader, resources) {
-    /**
-     * Textura fica disponível através do índice igual ao nome do arquivo de
-     * imagem
-     */
-    const sprite = new PIXI.Sprite(resources['images/megaman.png'].texture)
+const sprite = {}
+function setup () {
+  /**
+   * Textura fica disponível através do índice igual ao nome do arquivo de
+   * imagem.
+   */
+  sprite.arara = new PIXI.Sprite(PIXI.Loader.shared.resources[araraImg].texture)
 
-    /**
-     * Para renderizar o sprite ele precisa ser adicionado no objeto stage.
-     * Stage é o objeto que contem tudo que é renderizado.
-     */
-    app.stage.addChild(sprite);
-  }
+  /**
+   * Para renderizar o sprite ele precisa ser adicionado no objeto stage.
+   * Stage é o objeto que contem tudo que é renderizado.
+   */
+  app.stage.addChild(sprite.arara)
+
+  /**
+   * Carregando sprites from tileset (cartela de sprites). Nesse caso o
+   * tutorial apresenta esse carregamento de forma um pouco diferente da
+   * utilizada pelos sprites comuns.
+   * Nesse caso é realizada a identificação da localização do frame em
+   * que o sub-sprite está posicionado na textura. Assim o pixi realiza o
+   * "recorte" na textura.
+   */
+
+  // Obtêm a textura carregada
+  const megamanTexture = PIXI.utils.TextureCache[megamanImg]
+
+  // Identifica a localização do frame
+  megamanTexture.frame = new PIXI.Rectangle(164, 166, 52, 52)
+
+  // Carrega o "recorte" da textura como sprite
+  sprite.megaman = new PIXI.Sprite(megamanTexture)
+
+  // desloca a imagem para não sobrepor com a anterior da arara
+  sprite.megaman.x = 190
+  sprite.megaman.y = 32
+
+  /**
+   * Adição do sprite do tileset
+   */
+  app.stage.addChild(sprite.megaman);
+
+  /**
+   * Modificar posição do sprite
+   */
+  sprite.luigi = new PIXI.Sprite(PIXI.Loader.shared.resources[luigiImg].texture)
+
+  // redimencionamento com tamanho em pixels
+  sprite.luigi.width = 64
+  sprite.luigi.height = 64
+
+  // redimencionamento com porcentagem
+  // sprite.luigi.scale.set(0.5, 0.5)
+
+  // deslocamento com base no pixel 0,0 (canto superior esquerdo)
+  sprite.luigi.position.set(190, 190)
+
+  // define o ponto de rotação transladando a textura
+  //sprite.anchor.set(0.5, 0.5)
+
+  // define o ponto de rotação sem transportar a textura
+  sprite.luigi.pivot.set(32, 32)
+
+  // realiza a rotação [radianos]
+  sprite.luigi.rotation = 0.6
+
+  /**
+   * Inclui o sprite no stage para ser renderizado
+   */
+  app.stage.addChild(sprite.luigi)
+}
 
 
